3b372f9fbcb4bf7c9b4aa102e295068de7aaf5b5
Chiara Mizzi <chiara.mizzi@gmail.com>
2020-09-24 14:42:36 +0200

    add algo for fluxes of poly

--- src/slides_tools.cpp
+++ src/slides_tools.cpp
@@ -50,6 +50,58 @@ int main(int argc, char** argv)
 
     simulation s(jconf, &c);
     std::cout << s.info() << std::endl;
+
+    double dist_near = 100.0;
+    //finde near nodes
+    std::map<int, std::vector<int>> near_nodes;
+    for (const auto &ai : s.attractions) {
+      for (const auto &ni : c.node)
+        if (distance(c.node[ai.node_lid], ni) < dist_near)
+          near_nodes[ai.node_lid].push_back(ni.lid);
+    }
+
+    // make container and initialize
+    std::map<int, int> poly_counter;
+    for (const auto &pi : c.poly) {
+      poly_counter[pi.lid] = 0;
+    }
+    std::vector<int> path;
+    for (const auto &ni : near_nodes) {
+      for (const auto &nj : near_nodes) {
+        if (ni != nj) {
+          for (const auto &ai : ni.second)
+            for (const auto &aj : nj.second) {
+              path.clear();
+              if (c.bestpath(c.node[ai], c.node[aj], path) == BP_PATHFOUND)
+                for (const auto &pi : path)
+                  poly_counter[pi]++;
+            }
+        }
+      }
+
+
+      for (const auto &si : s.sources) {
+        for (const auto &bi:ni.second){
+        path.clear();
+        if (c.bestpath(c.node[bi], c.node[si.node_lid], path) == BP_PATHFOUND)
+          for (const auto &pi : path)
+            poly_counter[pi]++;
+        }
+      }
+    }
+    
+    //dump
+    std::string outname;
+    if (jconf.has_member("file_cnt_out")) {
+      std::ofstream out_fluxes(jconf["file_cnt_out"].as<std::string>());
+      out_fluxes << "#lid cid n_FT  n_TF" << std::endl;
+      for (const auto &pc : poly_counter)
+        out_fluxes << pc.first << " " << c.poly[pc.first].cid << " " << pc.second << " " << pc.second << std::endl;
+      out_fluxes.close();
+    }
+    else
+      std::cout << "file_cnt name unspecified in json config!" << std::endl;
+
   }
   catch (exception &e)
   {
