3f426273abf88ec8ffcd769ab97d2da669dd0bfc
Alessandro Fabbri <rommacc@yahoo.it>
2021-02-02 11:27:20 +0100

    add map maker for sybenik cameras

--- tools/sybenik/detection_engine.py
+++ tools/sybenik/detection_engine.py
@@ -90,12 +90,46 @@ class detection:
     except Exception as e:
       print(e)
 
+def make_cam_map(filename):
+  import pandas as pd
+  import folium
+
+  with open(filename) as jin:
+    camdata = json.load(jin)
+
+  df = pd.DataFrame.from_dict(camdata)
+  df['lat'] = df.coords.apply(lambda x: x[0])
+  df['lon'] = df.coords.apply(lambda x: x[1])
+  df['fake_name'] = df.name 
+  df.loc[ df.name.str.startswith('Centro'), 'fake_name' ] = 'Centro 1 & Centro 2'
+  df = df.drop(columns=['barriers', 'ip'])
+  center = df[['lat', 'lon']].mean()
+  print(df)
+  m = folium.Map(location=center, control_scale=True, zoom_start=9)
+  df.apply(lambda row: folium.CircleMarker(
+    location=[row.lat, row.lon], 
+    radius=8, 
+    fill_color='purple',
+    color='purple',
+    fill_opacity=1.,
+    popup=folium.Popup(f'camera {row["fake_name"]}', show=True, sticky=True),
+  ).add_to(m), axis=1)
+  s, w = df[['lat', 'lon']].min()
+  n, e = df[['lat', 'lon']].max()
+  m.fit_bounds([ [s,w], [n,e] ])
+  m.save(f'map_camera.html')
+
+
 if __name__ == '__main__':
   import argparse
   parser = argparse.ArgumentParser()
   parser.add_argument('-c', '--cfile', help='engine config json', required=True)
   args = parser.parse_args()
 
-  det = detection(configfile=args.cfile)
-  while True:
-    det.do_task()
+  if 0:
+    det = detection(configfile=args.cfile)
+    while True:
+      det.do_task()
+
+  if 1:
+    make_cam_map('../tools/sybenik/data/cam_data.json')
