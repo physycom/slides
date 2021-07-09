16b4b2489000f28f47db407e1948cbde472557ce
Fede <bellisardi@gmail.com>
2021-07-09 17:51:26 +0200

    edit plot and analysis for Thursday

--- tools/sybenik/camera_analysis.py
+++ tools/sybenik/camera_analysis.py
@@ -4,7 +4,9 @@ import os
 import json
 import argparse
 import numpy as np
+from numpy.core.numeric import full
 import pandas as pd
+import datetime as dt
 import mysql.connector
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -27,8 +29,7 @@ if __name__ == '__main__':
   start_date = config['start_date']
   stop_date  = config['stop_date']
 
-  start_date_ave = config['startdate']
-  stop_date_ave  = config['enddate']
+  location_map = {'Centro1':'Prolaz', 'Piazza':'TrgRH', 'Porto':'Riva', 'Centro2':'Poljana'}
 
   camera_map = ['Porto', 'Piazza', 'Centro1', 'Centro2']
   if pd.to_datetime(config['startdate'], format='%Y-%m-%d %H:%M:%S') < pd.to_datetime(config['start_date'], format='%Y-%m-%d %H:%M:%S'):
@@ -44,15 +45,19 @@ if __name__ == '__main__':
   print(f'Using {args.db} to get data from {data_inizio} to {data_fine}')
   print(f'Using {args.db} for analysis from {start_date} to {stop_date}')
 
+  def box_centered_kernel(tot_len, box_len):
+    pad_len = tot_len - box_len
+    kern = np.concatenate([
+    np.zeros((pad_len // 2)),
+    np.ones((box_len)) / box_len,
+    np.zeros((pad_len - pad_len // 2))# for odd box_len
+    ])
+    return kern
+  
   def data_analysis(df, output):
-    def box_centered_kernel(tot_len, box_len):
-      pad_len = tot_len - box_len
-      kern = np.concatenate([
-      np.zeros((pad_len // 2)),
-      np.ones((box_len)) / box_len,
-      np.zeros((pad_len - pad_len // 2))# for odd box_len
-      ])
-      return kern
+
+    comparison = f'{output}/comparison'
+    if not os.path.exists(comparison): os.mkdir(comparison)
 
     ave = df.copy()
 
@@ -153,6 +158,23 @@ if __name__ == '__main__':
 
       axxs.plot(np.unique(tsd), np.poly1d(np.polyfit(tsd, df_daily.SMOOTH, 1))(np.unique(tsd)), c = 'r', label = 'Trend')#), label = fr'Slope = {slope:.3f} $\pm$ {std_err:.3f} rad')
 
+      day_list = df_daily.SMOOTH.tolist()
+      changes = []
+      for x1, x2 in zip(day_list[:-1], day_list[1:]):
+        try:
+            x1 = day_list[0]
+            pct = (x2 - x1) * 100 / x1
+        except ZeroDivisionError:
+            pct = None
+        changes.append(pct)     
+      changes.insert(0,0)
+
+      df_daily['INCREMENT'] = changes
+
+      axxxs = axxs.twinx()
+      axxxs.plot(tsd, df_daily.INCREMENT)
+      axxxs.set_ylabel('Percentage of Increment from Day 1', color='b')
+
       axxs.legend()
       axxs.grid(which='major', linestyle='-')
       axxs.grid(which='minor', linestyle='--')
@@ -162,7 +184,7 @@ if __name__ == '__main__':
       # axxs.set_ylim(0, 70)
       plt.tight_layout()
       figg.subplots_adjust(top=0.9)
-      ptitle = f'Trend in "{locName}"'
+      ptitle = f'Trend in "{location_map[locName]}"'
       figg.suptitle(ptitle, y=0.98)
       trend = f'{output}/trend'
       if not os.path.exists(trend): os.mkdir(trend)
@@ -216,15 +238,18 @@ if __name__ == '__main__':
         ax.set_xticklabels(ts_lbl, rotation=45, ha='right')
         ax.set_ylabel('Counter')
         # ax.set_ylim(0, 50)
+        ax.set_xlabel('Daytime [Mon Week-Day HH:MM ]')
 
         plt.tight_layout()
         fig2.subplots_adjust(top=0.9)
-        ptitle = f'Comparison in "{locName}"\non {df_anal.index.date[0]}'
+        ptitle = f'Comparison in "{location_map[locName]}"\non {df_anal.index.date[0]}\nAve from {startdate.strftime("%Y-%m-%d")} to {enddate.strftime("%Y-%m-%d")}'
         plt.suptitle(ptitle, y=0.98)
 
-        comparison = f'{output}/comparison'
-        if not os.path.exists(comparison): os.mkdir(comparison)
-        plt.savefig(f'{comparison}/compare_{locName}_{df_anal.index.date[0].strftime("%Y%m%d")}_{wday}.png')
+        comparison_day = f'{comparison}/comparison_day'
+        if not os.path.exists(comparison_day): os.mkdir(comparison_day)
+        averanged = f'{comparison_day}/ave_{startdate.strftime("%Y%m%d")}_{enddate.strftime("%Y%m%d")}'
+        if not os.path.exists(averanged): os.mkdir(averanged)
+        plt.savefig(f'{averanged}/{locName}_{df_anal.index.date[0].strftime("%Y%m%d")}_{wday}.png')
 
         plt.close()
         fig2.clf()
@@ -281,6 +306,162 @@ if __name__ == '__main__':
     plt.clf()
     plt.close()  
 
+  def spec_analysis(df, output):
+    # Dal 08 Luglio al 02 Settembre plottare tutti i giovedì dalle 17.00 alle 21.00
+    # - rispetto a qualunque altro giorno della settimana nello stesso orario
+    # - rispetto alla media della stessa settimana
+
+    comparison = f'{output}/comparison'
+    if not os.path.exists(comparison): os.mkdir(comparison)
+
+    for locName, df_time in df.groupby(['LOC']):
+      df_time['hours'] = df_time.index.hour
+
+      start_hour = pd.to_datetime('17:00:00', format='%H:%M:%S')
+      end_hour = pd.to_datetime('21:00:00', format='%H:%M:%S')
+
+      mask = (df_time.hours >= start_hour.hour) & (df_time.hours < end_hour.hour)
+      df_time = df_time.loc[mask]
+
+      df_time = df_time.groupby(['DATETIME',  pd.Grouper(freq='300s')]).sum()
+      df_time = df_time.reset_index(level=[0])
+      df_time = df_time.drop(columns=['DATETIME', 'hours'])
+      df_time = df_time.groupby('DATETIME').sum()  
+      df_time['WEEK_NUMBER'] = df_time.index.isocalendar().week
+      for week_num, df_week in df_time.groupby(['WEEK_NUMBER']):
+        if week_num == 27:
+          df_week = df_week.drop(columns=['WEEK_NUMBER'])
+          mean_list = []
+          for time, df_hour in df_week.groupby([df_week.index.time]):
+            mean_list.append(df_hour.COUNTER.mean())
+
+          s_fake_date = pd.to_datetime('2021-01-01 17:00:00')
+          e_fake_date = pd.to_datetime('2021-01-01 20:59:59')
+          fullt = pd.date_range(start = s_fake_date,end= e_fake_date, freq=f'5min')
+          mean_df = pd.DataFrame(index=fullt)
+          mean_df['COUNTER'] = mean_list  
+          ma_size = 5 # running average idx interval from time in seconds
+          kern = box_centered_kernel(len(mean_df), ma_size)
+          conv = np.fft.fftshift(np.real(np.fft.ifft( np.fft.fft( mean_df.COUNTER ) * np.fft.fft(kern) )))
+          mean_df['SMOOTH'] = conv   
+
+          fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(16, 10))
+          fig2, axs2 = plt.subplots(nrows=2, ncols=1, figsize=(16, 10), sharex=True)
+          curves = []          
+
+          for data, df_data in df_week.groupby([df_week.index.date]):
+            ma_size = 5 # running average idx interval from time in seconds
+            kern = box_centered_kernel(len(df_data), ma_size)
+            conv = np.fft.fftshift(np.real(np.fft.ifft( np.fft.fft( df_data.COUNTER ) * np.fft.fft(kern) )))
+
+            df_data['SMOOTH'] = conv
+            df_data['TIME'] = df_data.index.time
+
+            df_data['plot_ts'] = [t.replace(year=2021, month=1, day=1) for t in df_data.index]
+
+            ts = [ t.timestamp() for t in df_data['plot_ts'] ]
+            tus = 3
+            lus = 1
+            ts_ticks = ts[::tus]
+            ts_lbl = [ t.strftime('%H:%M') for t in df_data['plot_ts'] ]
+            ts_lbl = ts_lbl[::tus]
+            ts_lbl = [ t if i%lus==0 else '' for i, t in enumerate(ts_lbl)]
+
+            wday = df_data.index.date[0].strftime('%a')
+
+            data_list = []
+            medie_list = []
+            diff_list = []
+            if wday == 'Thu':
+              data_list = df_data['SMOOTH'].tolist()
+              medie_list = mean_df['SMOOTH'].tolist() 
+              zip_obj = zip(data_list, medie_list)
+              for data_listi, medie_listi in zip_obj:
+                diff_list.append(data_listi - medie_listi)
+
+              ax = axs2[0]
+              ax.plot(ts, df_data.SMOOTH, '-o', label=f'{wday}, {df_data.index.date[0]}', markersize=4)
+              ax.plot(ts, mean_df.SMOOTH, '-o', label=f'Week Mean', markersize=4)
+              ax.legend()
+              ax.grid(which='major', linestyle='-')
+              ax.grid(which='minor', linestyle='--')
+              ax.set_xticks(ts_ticks)
+              ax.set_xticklabels(ts_lbl, rotation=45, ha='right')
+              ax.set_ylabel('Number of People [sampled 5 min]')
+
+              ax = axs2[1]
+              ax.plot(ts, diff_list, '-o', label=f'Plot Difference ', markersize=4)
+
+              ax.legend()
+              ax.grid(which='major', linestyle='-')
+              ax.grid(which='minor', linestyle='--')
+              ax.set_xticks(ts_ticks)
+              ax.set_xticklabels(ts_lbl, rotation=45, ha='right')
+              ax.set_ylabel('Differences')
+
+              ax.set_xlabel('Time [HH:MM]')
+
+              plt.tight_layout()
+              fig2.subplots_adjust(top=0.9)
+              ptitle = f'Comparison between Thuesday and weekly mean in {location_map[locName]}\nWeek: {week_num}'
+              plt.suptitle(ptitle, y=0.98)
+              if args.show:
+                plt.show()
+              else:
+                thisday = df_data.index.date[0].strftime('%Y%m%d')
+                compare_weekly = f'{output}/comparison/diff_week_{week_num}'
+                if not os.path.exists(compare_weekly): os.mkdir(compare_weekly)
+                plt.savefig(f'{compare_weekly}/diff_{locName}__week_num_{week_num}_{wday}_{thisday}.png')
+
+              plt.clf()
+              plt.close()
+
+            curve, = axs.plot(ts, df_data.SMOOTH, '-o', label=f'{wday}, {df_data.index.date[0]}', markersize=4)
+            curves.append(curve)
+  
+            leg = axs.legend()
+            leg.get_frame().set_alpha(0.4)
+            axs.grid(which='major', linestyle='-')
+            axs.grid(which='minor', linestyle='--')
+            axs.set_xticks(ts_ticks)
+            axs.set_xticklabels(ts_lbl, rotation=45, ha='right')
+            axs.set_ylabel('Number of People [sampled 5 min]')
+            axs.set_xlabel('Time [HH:MM]')
+  
+            curved = dict()
+            for legline, origline in zip(leg.get_lines(), curves):
+              legline.set_picker(5)
+              curved[legline] = origline
+  
+            def onpick(event):
+              legline = event.artist
+              origline = curved[legline]
+              vis = not origline.get_visible()
+              origline.set_visible(vis)
+  
+              if vis:
+                  legline.set_alpha(1.0)
+              else:
+                  legline.set_alpha(0.2)
+              fig.canvas.draw()
+  
+          fig.canvas.mpl_connect('pick_event', onpick)
+    
+          plt.tight_layout()
+          fig.subplots_adjust(top=0.9)
+          ptitle = f'Compare\nPeople in {location_map[locName]}, Week Number {week_num}'
+          plt.suptitle(ptitle, y=0.98)
+          if args.show:
+            plt.show()
+          else:
+            comparison = f'{output}/comparison'
+            all_week_day = f'{comparison}/week_data_in week_{week_num}'
+            if not os.path.exists(all_week_day): os.mkdir(all_week_day)
+            plt.savefig(f'{all_week_day}/week_num_{week_num}_{locName}.png')
+    
+          plt.clf()
+          plt.close()
+
   try:
    if args.db == 'mysql':
       conf = config['model_data']['params']['sybenik']['mysql']
@@ -321,7 +502,6 @@ if __name__ == '__main__':
           AND
           (BARRIER_UID in {tuple(camconv.keys())} )
       """
-      # print(query)
       tquery = datetime.now()
       cursor.execute(query)
       result = cursor.fetchall()
@@ -337,6 +517,8 @@ if __name__ == '__main__':
       df = df.drop(columns='BARRIER_UID')
 
       data_analysis(df, base_save)
+
+      spec_analysis(df, base_save)
 
   except Exception as e:
     print('Connection error : {}'.format(e))
